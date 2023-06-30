6c44bc19c0568856471494435582c9a3f10df8de
Sam <stmiyamoto10@gmail.com>
2023-06-30 09:16:12 -0700

    Adding test file

--- tests.py
+++ tests.py
@@ -15,6 +15,25 @@ class FlaskAppTests (unittest.TestCase):
         result = self.client.get('/')
         self.assertIn(b'<h4>Create an Account</h4>', result.data)
 
+    
+    def test_create_user(self):
+        """Test that a user is created with correct data types."""
+
+        # result = self.client.post("/users",
+        #          data={
+        #         "fname": "Tabitha",
+        #         "city": "Fargo",
+        #         "country_code": "US",
+        #         "phone": 555-555-8888,
+        #         "email": tab@test.xyz,
+        #  
+
+        #           },
+
+        #           follow_redirects=True)
+
+        # self.assertIn(b"Your account was created successfully! You can now log in.", result.data)
+
 
 if __name__ == '__main__':
     # If called like a script, run the tests
